27a63110bdc8c3e19d0220465b3c0157d76f27ae
manu <IVIanuu@gmail.com>
2023-01-23 19:34:30 +0100

    ...

--- app/src/main/kotlin/com/ivianuu/soundboks/domain/SoundboksRemote.kt
+++ app/src/main/kotlin/com/ivianuu/soundboks/domain/SoundboksRemote.kt
@@ -100,7 +100,7 @@ class SoundboksServer(address: String) {
   val device = adapter.getRemoteDevice(address)
 
   private val sendLock = Mutex()
-  private val sendLimiter = RateLimiter(1, 25.milliseconds)
+  private val sendLimiter = RateLimiter(1, 100.milliseconds)
 
   private val gatt = adapter
     .getRemoteDevice(address)
