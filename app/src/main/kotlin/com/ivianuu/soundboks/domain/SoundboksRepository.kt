b6d83d8f1d3cfb7eacdcd1cb03662dfd2c9943d1
manu <IVIanuu@gmail.com>
2023-04-24 12:44:02 +0200

    Revert "Fix"
    This reverts commit ffcf3b77f10951cf09e56f1922144f9568bc2686.

--- app/src/main/kotlin/com/ivianuu/soundboks/domain/SoundboksRepository.kt
+++ app/src/main/kotlin/com/ivianuu/soundboks/domain/SoundboksRepository.kt
@@ -32,6 +32,7 @@ import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
@@ -69,27 +70,21 @@ import kotlinx.coroutines.sync.withLock
   private fun bleSoundbokses(): Flow<List<Soundboks>> = callbackFlow<List<Soundboks>> {
     val soundbokses = mutableListOf<Soundboks>()
 
-    fun handleSoundboks(soundboks: Soundboks, isConnected: Boolean) {
+    fun handleSoundboks(soundboks: Soundboks) {
       launch {
-        suspend fun add() {
-          logger { "${soundboks.debugName()} add soundboks" }
-          soundbokses += soundboks
-          trySend(soundbokses.toList())
-        }
-
         soundboksLock.withLock {
           foundSoundbokses += soundboks
           if (soundbokses.any { it.address == soundboks.address })
             return@launch
-          else if (isConnected)
-            add()
+          else {
+            logger { "${soundboks.debugName()} add soundboks" }
+            soundbokses += soundboks
+            trySend(soundbokses.toList())
+          }
         }
 
-        remote.withSoundboks(soundboks.address) {
-          onCancel(block = {
-            if (!isConnected) add()
-            awaitCancellation()
-          }) {
+        remote.withSoundboks<Unit>(soundboks.address) {
+          onCancel {
             logger { "${soundboks.debugName()} remove soundboks" }
             soundboksLock.withLock {
               soundbokses.removeAll { it.address == soundboks.address }
@@ -102,15 +97,15 @@ import kotlinx.coroutines.sync.withLock
 
     bluetoothManager.getConnectedDevices(BluetoothProfile.GATT)
       .filter { it.isSoundboks() }
-      .forEach { handleSoundboks(it.toSoundboks(), true) }
+      .forEach { handleSoundboks(it.toSoundboks()) }
 
-    foundSoundbokses.forEach { handleSoundboks(it, false) }
+    foundSoundbokses.forEach { handleSoundboks(it) }
 
     val callback = object : ScanCallback() {
       override fun onScanResult(callbackType: Int, result: ScanResult) {
         super.onScanResult(callbackType, result)
         if (result.device.isSoundboks())
-          handleSoundboks(result.device.toSoundboks(), true)
+          handleSoundboks(result.device.toSoundboks())
       }
     }
 
