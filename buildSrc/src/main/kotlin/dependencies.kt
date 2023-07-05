097309052d6f93d0331b03beba928c318849345b
manu <ivianuu@gmail.com>
2023-07-05 10:08:10 +0200

    ...

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -15,7 +15,7 @@ object Build {
 
 object Deps {
   object Essentials {
-    private const val version = "0.0.1-dev1197"
+    private const val version = "0.0.1-dev1198"
     const val android = "com.ivianuu.essentials:essentials-android:$version"
     const val broadcast = "com.ivianuu.essentials:essentials-broadcast:$version"
     const val gradlePlugin = "com.ivianuu.essentials:essentials-gradle-plugin:$version"
